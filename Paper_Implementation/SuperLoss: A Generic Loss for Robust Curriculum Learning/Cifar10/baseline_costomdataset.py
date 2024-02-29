034e795d4f91c251510f6943dc0abe72b9813b9f
Gun-Hee-SON <106459423+NICESONY@users.noreply.github.com>
2024-02-29 22:14:45 +0900

    Add files via upload

---
+++ Paper_Implementation/SuperLoss: A Generic Loss for Robust Curriculum Learning/Cifar10/baseline_costomdataset.py
@@ -0,0 +1,42 @@
+import torch
+from torch.utils.data import DataLoader, Subset
+from torchvision.transforms import transforms
+import torchvision.datasets as datasets
+import numpy as np
+
+
+
+
+def prepare_dataset():
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(0.5, 0.5)
+    ])
+
+    data_root = "/media/syoon/DATA2/2024_WINTTER_RAB/baseline_code/cifar10_code/data"
+
+    train_set_all = datasets.CIFAR10(root = data_root, train = True, download = True, transform = transform) 
+    test_set = datasets.CIFAR10(root = data_root, train = False, download = True, transform = transform)
+
+
+    train_size = len(train_set_all)
+    dev_size = int(0.2 * train_size)
+
+
+    idxes = list(range(train_size))
+    np.random.shuffle(idxes)
+
+
+    train_set_idx = idxes[dev_size :]
+    dev_set_idx = idxes[:dev_size]
+
+
+    train_set = Subset(train_set_all, train_set_idx)
+    dev_set = Subset(train_set_all, dev_set_idx)
+
+
+    batch_size = 128
+    train_loader = DataLoader(train_set, batch_size = batch_size, shuffle = True) 
+    dev_loader = DataLoader(dev_set, batch_size = batch_size, shuffle = False)
+    test_loader = DataLoader(test_set, batch_size = batch_size, shuffle = False)
+    return train_loader, dev_loader ,test_loader
---
+++ Paper_Implementation/SuperLoss: A Generic Loss for Robust Curriculum Learning/Cifar10/baseline_utils.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+
+
+
+
+def torch_seed(seed = 42):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.use_determinstic_algoriths = True
+
+
+
+
+
+
+
+def evaluate_history(history):
+    print(f"inital state val_loss : {history[0,2]:.5f}"
+          ,f"inital stast val_acc :  {history[0,4] :.5f}")
+    print(f"final state val_loss : {history[-1,2]:.5f}"
+          ,f"final stast val_acc :  {history[-1,4] :.5f}")
+
+    # 최고 정확도와 최소 손실 찾기
+    best_accuracy = max(history[:, 4])
+    best_loss = min(history[:, 2])
+    print(f"Best accuracy : {best_accuracy:.5f}")
+    print(f"Best loss : {best_loss:.5f}")
+
+    num_epochs = len(history)
+    unit = num_epochs / 10
+    plt.figure(figsize=(9,8))
+    plt.plot(history[:,0], history[:,1], "b",label = "train")
+    plt.plot(history[:, 0], history[:, 3], "k", label="val")
+    plt.xticks(np.arange(0, num_epochs+1, unit))
+    plt.xlabel("epoch")
+    plt.ylabel("loss")
+    plt.title("learning curve(loss)")
+    plt.legend()
+    plt.show()
+
+    plt.figure(figsize=(9,8))
+    plt.plot(history[:, 0], history[:,2], "b", label = "train")
+    plt.plot(history[:, 0], history[:, 4], "k", label = "val")
+    plt.xticks(np.arange(0, num_epochs+1 , unit))
+    plt.xlabel("epoch")
+    plt.ylabel("acc")
+    plt.title("learning curve(acc)")
+    plt.legend()
+    plt.show()
